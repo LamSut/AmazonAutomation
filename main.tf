8d33a47d4f3fa0a5d7740b17c1a3cb7adf761806
LamSut <lamsut0007@gmail.com>
2025-07-11 22:32:13 +0700

    reconstruct

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
-  source = "./vpc"
+  source = "./tf-modules/vpc"
 }
 
 module "ec2" {
-  source         = "./ec2"
+  source         = "./tf-modules/ec2"
   subnet1        = module.vpc.public_subnet1
   subnet2        = module.vpc.public_subnet2
   security_group = module.vpc.security_group
